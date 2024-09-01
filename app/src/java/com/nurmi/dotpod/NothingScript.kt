31ccf36113542858d6940ca02706000820326240
NurMi App <sasha.rostovskaya@bk.ru>
2024-09-01 11:19:46 +0300

    Add NothingScript.kt for device control.

--- app/src/java/com/nurmi/dotpod/NothingScript.kt
+++ app/src/java/com/nurmi/dotpod/NothingScript.kt
@@ -1,26 +1,7 @@
-import android.graphics.Color
-import android.os.Bundle
-import android.view.View
-import androidx.appcompat.app.AppCompatActivity
-import kotlin.random.Random
+package com.nurmi.dotpod
 
-class MainActivity : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        
-        val view = findViewById<View>(R.id.colorView) // colorView는 색상을 변경할 뷰의 ID입니다.
-
-        view.setOnClickListener {
-            // 랜덤 색상 생성
-            val randomColor = Color.argb(
-                255, // 알파값
-                Random.nextInt(256), // 빨간색 성분
-                Random.nextInt(256), // 초록색 성분
-                Random.nextInt(256) // 파란색 성분
-            )
-            view.setBackgroundColor(randomColor) // 배경색 설정
-        }
+object NothingScript {
+    fun controlDevice() {
+        println("Device control logic here")
     }
 }
