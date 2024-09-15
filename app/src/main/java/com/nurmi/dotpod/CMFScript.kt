9d7559f88c9226a3cca5a3bc2727771c7395b024
Nurkhametov Mike <sasha.rostovskaya@bk.ru>
2024-09-15 13:58:28 +0300

    New banner ads on preference.

--- app/src/main/java/com/nurmi/dotpod/CMFScript.kt
+++ app/src/main/java/com/nurmi/dotpod/CMFScript.kt
@@ -3,5 +3,14 @@ package com.nurmi.dotpod
 object CMFScript {
     fun activateFeatures() {
         println("Activating features for CMF Buds")
+
+        fun createBluetoothFAB() {
+  MaterialComponents.FloatingActionButton {
+   setOnClickListener {
+  // Perform Bluetooth actions
+       val bannerAd = AdMobAds.bannerAd(adUnitId = "YourAdUnitID")
+     }
+  }
+        }
     }
 }
